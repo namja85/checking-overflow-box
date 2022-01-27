13aa562a32f34f6da9f34b66b37eadab5bdeff18
GyeongeunHyun <gehyun@osstem.com>
2022-01-27 17:26:34 +0900

    add flex-grow in inner box

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const right = document.querySelector('.right');
 
 const randomHeightBoxCount = 40;
 const randomHeightBoxes = Array.from({ length: randomHeightBoxCount }, (_, index) => index);
-const moveLeft = 250 * 3 + 4 * 2 + 4; // inner width(250px) * 3columns + gap(4px) * 2 + padding of one side(4px);
+const moveLeft = 250 * 3 + 4 * 2 + 4; // inner width(250px) * 3columns + gap(4px) * 2 + gap of next page(4px);
 
 randomHeightBoxes.forEach(elem => {
   const inner = document.createElement('div');
@@ -20,12 +20,15 @@ randomHeightBoxes.forEach(elem => {
 });
 
 add.addEventListener('click', () => {
-  const inner = document.createElement('div');
-  inner.setAttribute('class', 'inner');
-  box.appendChild(inner);
-  // console.log(box);
-  console.dir(box);
-  console.log(isOverflown(box));
+  // const inner = document.createElement('div');
+  // inner.setAttribute('class', 'inner');
+  // box.appendChild(inner);
+  // // console.log(box);
+  // console.dir(box);
+  // console.log(isOverflown(box));
+  for (let i = 0; i < box.children.length; i++) {
+    box.children[i].classList.add('grow');
+  }
 });
 
 left.addEventListener('click', () => {
@@ -44,7 +47,7 @@ right.addEventListener('click', () => {
   showButtons();
 });
 
-console.log(isOverflown(box));
+// console.log(isOverflown(box));
 
 function showButtons() {
   if (isOverflown(box)) {
@@ -60,3 +63,36 @@ function showButtons() {
 }
 
 showButtons();
+
+document.addEventListener('DOMContentLoaded', () => {
+  let column = 0;
+  let sumHeight = 0;
+
+  for (let i = 0; i < box.children.length; i++) {
+    if (sumHeight + box.children[i].offsetHeight > 500) {
+      column++;
+      sumHeight = 0;
+    }
+    sumHeight += box.children[i].offsetHeight;
+    // console.log(i, box.children[i].offsetHeight, column);
+  }
+
+  while (true) {
+    if (sumHeight + 200 < 500) {
+      const element = document.createElement('div');
+      element.setAttribute('class', 'inner dummy');
+      // element.textContent = 'dummy';
+      box.appendChild(element);
+      sumHeight += element.offsetHeight; 
+      // console.log(sumHeight, element.offsetHeight, sumHeight + element.offsetHeight);
+    } else {
+      break;
+    }
+  }
+
+  for (let i = 0; i < box.children.length; i++) {
+    box.children[i].classList.add('grow');
+  }
+
+  // console.dir(box);
+});
